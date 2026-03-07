361778097dc440880e08536e965172e24a95b333
Grayson Gilbert <graysongilbert@gmail.com>
2026-03-07 18:00:41 -0500

    Added teleop functionality

---
+++ launch/teleop.launch.py
@@ -0,0 +1,47 @@
+
+import os
+import launch
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.webots_controller import WebotsController
+
+
+
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, GroupAction
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    
+    # Locate robot files
+    webots_package_dir = get_package_share_directory('walker')
+    robot_description_path = os.path.join(webots_package_dir, 'resource', 'my_robot.urdf')
+    
+    
+    robot_driver_node = WebotsController(
+        robot_name='walker_robot',
+        parameters=[{'robot_description': robot_description_path},]
+    )
+
+    # Webots world
+    webots = WebotsLauncher(
+        world=os.path.join(webots_package_dir, 'worlds', 'my_world.wbt')
+    )
+
+
+    return LaunchDescription([
+        webots,
+        robot_driver_node,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+            
+        )
+    ])
---
+++ src/teleop.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import sys, select, termios, tty
+
+class TeleOpNode(Node):
+    def __init__(self):
+        # Initialize the node with a name
+        super().__init__('TeleopNode')
+        
+        # Create a publisher on the /cmd_vel topic
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.settings = termios.tcgetattr(sys.stdin)
+        #self.run_teleop()
+        
+        # Create a timer that calls the callback every 0.1 seconds (10Hz)
+        self.timer_period = 0.1  
+        self.timer = self.create_timer(self.timer_period, self.teleop_callback)
+        
+        self.get_logger().info('TeleOp Node has started!')
+        
+        self.accepted_keys = ['w', 's', 'd', 'a', ' ']
+
+    def teleop_callback(self):
+        msg = Twist()
+        key = self.get_key_input()
+        
+        if key == "\x03":
+            raise KeyboardInterrupt
+        if key in self.accepted_keys:
+            
+            if key == "w":
+                msg.linear.x = 0.5
+                msg.angular.z = 0.0
+                self.get_logger().info(f'Key Pressed: {key} Linear={msg.linear.x}, Angular={msg.angular.z}')
+                
+            elif key == "s":
+                msg.linear.x = -0.5
+                msg.angular.z = 0.0
+                self.get_logger().info(f'Key Pressed: {key} Linear={msg.linear.x}, Angular={msg.angular.z}')
+                
+            elif key == "a":
+                msg.linear.x = 0.0
+                msg.angular.z = 0.5
+                self.get_logger().info(f'Key Pressed: {key} Linear={msg.linear.x}, Angular={msg.angular.z}')
+                
+            elif key == "d":
+                msg.linear.x = 0.0
+                msg.angular.z = -0.5
+                self.get_logger().info(f'Key Pressed: {key} Linear={msg.linear.x}, Angular={msg.angular.z}')
+                
+            else:
+                msg.linear.x = 0.0
+                msg.angular.z = 0.0
+                self.get_logger().info(f'Key Pressed: {key} Linear={msg.linear.x}, Angular={msg.angular.z}')
+
+        else:
+            self.get_logger().info(f'Key not accepted! Press one of the following keys: [a,s,w,d, or space bar]')
+            
+        # Publish the message
+        if rclpy.ok():
+            self.publisher_.publish(msg)
+            
+
+    def get_key_input(self):
+        
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys. stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)   
+        
+        return key     
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TeleOpNode()
+    
+    try:
+        rclpy.spin(node)
+    except (KeyboardInterrupt):
+        pass
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, node.settings)
+        node.get_logger().info('Shutting down...')
+        if rclpy.ok():
+            node.destroy_node()
+            rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
